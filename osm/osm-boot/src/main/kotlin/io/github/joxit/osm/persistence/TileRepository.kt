191e308519689f4c2562c84561214c7c51bdb35d
Joxit <joxit972@gmail.com>
2023-10-01 21:34:28 +0200

    feat(osm): move utils.SVG to persistence.TileRepository

--- osm/osm-boot/src/main/kotlin/io/github/joxit/osm/persistence/TileRepository.kt
+++ osm/osm-boot/src/main/kotlin/io/github/joxit/osm/persistence/TileRepository.kt
@@ -1,54 +1,53 @@
-package io.github.joxit.osm.utils
+package io.github.joxit.osm.persistence
 
 import io.github.joxit.osm.model.Tile
+import java.awt.image.BufferedImage
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import javax.imageio.ImageIO
+import kotlin.math.pow
 import org.apache.batik.anim.dom.SAXSVGDocumentFactory
 import org.apache.batik.anim.dom.SVGOMSVGElement
 import org.apache.batik.transcoder.TranscoderInput
 import org.apache.batik.transcoder.TranscoderOutput
 import org.apache.batik.transcoder.image.ImageTranscoder
 import org.apache.batik.util.XMLResourceDescriptor
+import org.springframework.core.io.Resource
 import org.w3c.dom.Document
-import java.awt.image.BufferedImage
-import java.io.ByteArrayOutputStream
-import java.io.IOException
-import javax.imageio.ImageIO
-import kotlin.math.pow
 
-object Svg {
+class TileRepository(private val worldSVG: Resource) {
   private fun getWorldSVG(): Document {
     try {
       return SAXSVGDocumentFactory(XMLResourceDescriptor.getXMLParserClassName())
-          .createDocument(null, Svg::class.java.classLoader.getResourceAsStream("world.svg"))
+        .createDocument(null, worldSVG.inputStream)
     } catch (e: IOException) {
       throw IllegalStateException(e)
     }
   }
 
-  @JvmStatic
-  fun getTile(t: Tile): ByteArray {
-    return svgToPng(getWorldSVG().tile(t.z, t.x, t.y), 256)
+  fun generateTile(tile: Tile): ByteArray {
+    return getWorldSVG().tile(tile).toPng(256)
   }
 
-  fun Document.tile(z: Int, x: Int, y: Int): Document {
+  private fun Document.tile(t: Tile): Document {
     val elt = this.documentElement as SVGOMSVGElement
-    val divider = 2.toFloat().pow(z)
+    val divider = 2.toFloat().pow(t.z)
     val size = elt.viewBox.baseVal.height / divider
 
-    elt.viewBox.baseVal.x += size * x
-    elt.viewBox.baseVal.y += size * y
+    elt.viewBox.baseVal.x += size * t.x
+    elt.viewBox.baseVal.y += size * t.y
     elt.viewBox.baseVal.height = size
     elt.viewBox.baseVal.width = size
     return this
   }
 
-  @JvmStatic
-  fun svgToPng(svg: Document, size: Int = 256): ByteArray {
+  private fun Document.toPng(size: Int = 256): ByteArray {
     val transcoder = CustomTranscoder()
     val writer = ByteArrayOutputStream()
 
     transcoder.addTranscodingHint(ImageTranscoder.KEY_WIDTH, size.toFloat())
     transcoder.addTranscodingHint(ImageTranscoder.KEY_HEIGHT, size.toFloat())
-    transcoder.transcode(TranscoderInput(svg), null)
+    transcoder.transcode(TranscoderInput(this), null)
 
     ImageIO.write(transcoder.image, "PNG", writer)
     return writer.toByteArray()
